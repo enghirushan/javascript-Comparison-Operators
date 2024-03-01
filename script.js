9c744ed0404769b74981c89b0cf3bac2b6dd859a
Ilangasinghe I M H H <102861747+enghirushan@users.noreply.github.com>
2024-03-01 15:03:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let x =30;
+let y =40;
+
+//   == operator
+console.log(x==y);
+
+//   === operator
+ console.log(x==y);
+
+//   != operator
+console.log(x!=y);
+
+//   !== operator
+console.log(x!=y);
+
+//   > operator
+console.log(x>y);
+
+//   < operator
+console.log(x<y);
+
+//   >= operator
+console.log(x>=y);
+
+//   <= operator
+console.log(x<=y);
+
+//   ? operator (ternary)
+console.log(x>y ? "x is greater than y" : "y is greater than x");
+ 
